d7c753d3f9c0b23344c5503863c67fda34adedbc
saarangbond <48871052+saarangbond@users.noreply.github.com>
2020-06-17 14:11:40 -0700

    Add files via upload

---
+++ build/precache-manifest.57c3ab94713ed772a30f79ada5941193.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c1a28ec132688e8dcbfd195ffd0e64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9976092038c6aa3a7640",
+    "url": "/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "866b4afacbe6f30e4af7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9976092038c6aa3a7640",
+    "url": "/static/js/2.7add86fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7add86fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "866b4afacbe6f30e4af7",
+    "url": "/static/js/main.14c83466.chunk.js"
+  },
+  {
+    "revision": "62e5aac5ebe50afc9fe6",
+    "url": "/static/js/runtime-main.0ee0ab35.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/components/navbar.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable */
+
+import React, { Component } from "react";
+
+//Stateless Functional Component
+const NavBar = ({ totalCounters }) => {
+  console.log("NavBar - Rendered");
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
